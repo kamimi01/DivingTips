65b6020e5ca92bfc0840607421cf217b6eda4f33
mika.urakawa <awakaru0122@gmail.com>
2022-08-15 15:53:22 +0900

    Initial Commit

---
+++ DivingTips/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DivingTips
+//
+//  Created by mikaurakawa on 2022/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DivingTips/DivingTipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivingTipsApp.swift
+//  DivingTips
+//
+//  Created by mikaurakawa on 2022/08/15.
+//
+
+import SwiftUI
+
+@main
+struct DivingTipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
