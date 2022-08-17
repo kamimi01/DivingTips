07e110795f15f08659e71fb849ba49bfbc7be971
mika.urakawa <awakaru0122@gmail.com>
2022-08-17 23:11:27 +0900

    add: アカウント画面の追加

--- DivingTips/Account/AccountView.swift
+++ DivingTips/Account/AccountView.swift
@@ -8,8 +8,78 @@
 import SwiftUI
 
 struct AccountView: View {
+    @Environment(\.openURL) var openURL
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section {
+                    profileView
+                }
+                Section {
+                    tipsList
+                }
+                Section {
+                    termsAndUseView
+                    privacyPolicyView
+                }
+                Section {
+                    logoutButton
+                }
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("アカウント")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+private extension AccountView {
+    var profileView: some View {
+        HStack {
+            Image(systemName: "person")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 60, height: 60)
+                .cornerRadius(75)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 75).stroke(Color.black, lineWidth: 1)
+                )
+                .padding(.trailing, 10)
+            Text("kamimi01")
+        }
+        .foregroundColor(.black)
+        .padding(.vertical, 10)
+    }
+    
+    var tipsList: some View {
+        NavigationLink(destination: HomeView()) {
+            Text("コツ一覧")
+        }
+    }
+    
+    var logoutButton: some View {
+        Button(action: {}) {
+            Text("ログアウト")
+        }
+        .foregroundColor(.black)
+    }
+    
+    var termsAndUseView: some View {
+        Button(action: {
+            openURL(URL(string: "https://kamimi01.github.io/ArticleCollection/privacy_policy/ja.html")!)
+        }) {
+            Text("利用規約・プライバシーポリシー")
+        }
+        .foregroundColor(.black)
+    }
+    
+    var privacyPolicyView: some View {
+        HStack {
+            Text("バージョン")
+            Spacer()
+            Text(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "")
+        }
     }
 }
 
